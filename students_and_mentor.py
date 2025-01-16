99dd392cef628766572606d1aec2a50b4a3359f8
Vadim.F <fedotovvadik@gmail.com>
2025-01-17 02:11:01 +0500

    add Lecturer & Reviever

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,6 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    # Создаём метод выставления оценок лекторам
+    def rate_Lect(self, lecturer, course, grade_Lect):
+            if isinstance(lecturer, Lecturer(Mentor)) and course in self.courses_attached and course in student.courses_in_progress:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
+            else:
+                return 'Ошибка'
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -15,21 +25,24 @@ class Mentor:
         
 
         
-# Создаём класс-наследник от Mentor - Lecturer (лекторы)
-class Lecturer(Mentor):
+    # Задание № 1. Создаём класс-наследник от Mentor - Lecturer (лекторы)
+    class Lecturer(Mentor):
+        def __init__(self):
+            self.grades_Lect = {}
 
 
-# Создаём класс-наследник от Mentor - Reviewer (эксперты, проверяющие домашние задания)
-class Reviewer(Mentor):
-    # Объявляем метод выставления оценок студентам
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+    # Задание № 1. -Создаём класс-наследник от Mentor - Reviewer (эксперты, проверяющие домашние задания)
+    class Reviewer(Mentor):
+        def __init__(self):
+        # Создаём метод выставления оценок студентам
+        def rate_hw(self, student, course, grade):
+            if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
             else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+                return 'Ошибка'
 
 
 
@@ -44,3 +57,46 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
+
+
+# class Student:
+#     def __init__(self, name, surname, gender):
+#         self.name = name
+#         self.surname = surname
+#         self.gender = gender
+#         self.finished_courses = []
+#         self.courses_in_progress = []
+#         self.grades = {}
+
+#         def add_courses(self, course_name):
+#    		    self.finished_courses.append(course_name)   
+
+     
+# class Mentor:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#         self.courses_attached = []
+        
+#     def rate_hw(self, student, course, grade):
+#         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+#             if course in student.grades:
+#                 student.grades[course] += [grade]
+#             else:
+#                 student.grades[course] = [grade]
+#         else:
+#             return 'Ошибка'
+
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python', 'Java']
+
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python', 'Java']
+
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Java', 10)
+# cool_mentor.rate_hw(best_student, 'Java', 10)
+
+# print(best_student.grades)
